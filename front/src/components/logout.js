d0ace722d423360637ec8550df4475be323ae9f8
Phoenix-Melchor <johan_melchor@hotmail.com>
2025-08-04 20:23:12 -0600

    [ADD] Se agregaron estilos en los botnes de login y register y menu de log out

--- front/src/components/logout.js
+++ front/src/components/logout.js
@@ -1,14 +1,28 @@
 import React from "react";
 import styles from '../styles/logout.module.css';
+import { deleteToken } from "../utils/tokenUtils";
 
 function Logout() {
+
+    const handleLogout = () => {
+        deleteToken();
+        window.location.reload();
+        window.location.href = '/login';
+    }
+
+    const handleCancel = () => {
+        window.location.href = '/';
+    }
 
     return (
         <div className={styles['main-container']}>
             <div className={styles['logout-form']}>
                 <h2>Cerrar Sesión</h2>
-                <button>Si</button>
-                <button>No</button>
+                <h3>¿Estás seguro que deseas cerrar sesión?</h3>
+                <div className={styles.buttons}>
+                    <button onClick={handleLogout}>Si</button>
+                    <button onClick={handleCancel}>No</button>
+                </div>
             </div>
         </div>
     )
